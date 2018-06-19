162a84f37ea1e1d544edb199e22a5e2f5603bb4f
Juanjo Soriano <23655794+ewolvy@users.noreply.github.com>
2018-06-19 18:56:37 +0200

    Remove ignored files

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="df8e0bee-7f3b-44d8-b9d4-b1ca27306210")
+public class DataBindingInfo {}
